a433aeeb7b657a030f7663573bc4a47064094a25
hanshuai <hanmelon2013@163.com>
2022-03-30 21:12:52 +0800

    新建分支

---
+++ app/src/main/java/com/hanchao/gitworkdemo/HanActivity.java
@@ -0,0 +1,14 @@
+package com.hanchao.gitworkdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_han);
+    }
+}
